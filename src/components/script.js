d811b7397babdef866b49f74dce9d3533859e6c5
Giancarlo Sanchez <gianksp@gmail.com>
2022-12-08 08:42:36 +0000

    Fix ternary

--- src/components/script.js
+++ src/components/script.js
@@ -14,6 +14,16 @@ async function script(props) {
     },
   ];
 
+  const getAccount = () => {
+    const provider = window.walletProvider;
+    if (!provider) return;
+
+    if (provider.accounts && provider.accounts.length > 0)
+      return provider.accounts[0];
+    
+    return provider.selectedAddress;
+  };
+
   const hasPositiveBalance = async () => {
     if (!window.walletProvider) return false;
     let numericBalance = 0;
@@ -23,11 +33,7 @@ async function script(props) {
       const provider = new ethers.providers.Web3Provider(window.walletProvider);
       const signer = provider.getSigner();
       const contract = new ethers.Contract(props.contract, abi, signer);
-      const account =
-        window.walletProvider?.accounts &&
-        window.walletProvider?.accounts?.length > 0
-          ? window.walletProvider?.accounts[0]
-          : window.walletProvider.selectedAddress;
+      const account = getAccount();
       const balance = await contract.balanceOf(account);
       numericBalance = balance.toNumber();
     } catch (e) {
